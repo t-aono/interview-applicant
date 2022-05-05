28e7dd8901e3030e2549b4f914766e2c71c545fc
t-aono <info@t-aono.net>
2022-05-05 13:05:18 +0900

    create  media url pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { LoginComponent } from './login/login.component';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { DoneComponent } from './done/done.component';
+import { MediaUrlPipe } from './pipe/media-url.pipe';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,7 @@ import { DoneComponent } from './done/done.component';
     DetailComponent,
     LoginComponent,
     DoneComponent,
+    MediaUrlPipe,
   ],
   imports: [
     BrowserModule,
