d13c86a429e75287769df17472783fb5775f9b02
t-aono <info@t-aono.net>
2022-05-06 21:38:33 +0900

    make setting service

--- src/app/service/setting.service.ts
+++ src/app/service/setting.service.ts
@@ -12,16 +12,39 @@ import { OriginalForm } from 'app/model/form';
 export class SettingService {
   private formsCollection: AngularFirestoreCollection<OriginalForm>;
   forms$: Observable<OriginalForm[]>;
+  formsCount = 0;
 
   constructor(private afs: AngularFirestore) {
     this.formsCollection = afs.collection<OriginalForm>('forms');
     this.forms$ = this.formsCollection.snapshotChanges().pipe(
       map((actions) =>
-        actions.map((a) => {
-          const data = a.payload.doc.data() as OriginalForm;
-          return data;
-        })
+        actions
+          .map((a) => {
+            const data = a.payload.doc.data() as OriginalForm;
+            const id = a.payload.doc.id;
+            return { id, ...data };
+          })
+          .sort((a, b) => a.key - b.key)
       )
+    );
+  }
+
+  addForm(form: OriginalForm) {
+    return this.formsCollection.add(form);
+  }
+
+  deleteForm(deleteKey: number) {
+    this.forms$.subscribe((forms) =>
+      forms.forEach((form) => {
+        if (form.key === deleteKey) {
+          this.formsCollection.doc(form.id).update({
+            key: form.key,
+            label: form.label,
+            name: form.name,
+            isValid: false,
+          });
+        }
+      })
     );
   }
 }
