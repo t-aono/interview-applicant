f6244caff29220eecacd81a5961c2b3ed39cbdf6
t-aono <info@t-aono.net>
2022-05-06 21:38:33 +0900

    make setting service

--- src/app/component/setting/setting.component.ts
+++ src/app/component/setting/setting.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { OriginalForm } from 'app/model/form';
 import { SettingService } from 'app/service/setting.service';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'ia-setting',
@@ -10,11 +10,36 @@ import { Observable } from 'rxjs';
 })
 export class SettingComponent implements OnInit {
   forms$: Observable<OriginalForm[]>;
+  formsSubscription: Subscription;
+  targetForm: OriginalForm = {
+    key: 0,
+    label: '',
+    name: '',
+    isValid: true,
+  };
 
   constructor(private settingService: SettingService) {
     this.forms$ = this.settingService.forms$;
-    // this.forms$.subscribe((v) => console.log(v));
+    this.targetForm.key = this.settingService.formsCount + 1;
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.formsSubscription = this.forms$.subscribe(
+      (forms) => (this.targetForm.key = forms.length + 1)
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.formsSubscription.unsubscribe();
+  }
+
+  saveForm() {
+    this.settingService.addForm(this.targetForm);
+  }
+
+  deleteForm(key) {
+    if (window.confirm('非表示にしますか？')) {
+      this.settingService.deleteForm(key);
+    }
+  }
 }
