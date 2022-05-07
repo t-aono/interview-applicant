4704efe251bfaf79d2d194405574daac4ffda748
t-aono <info@t-aono.net>
2022-05-07 13:11:11 +0900

    add form group

--- src/app/component/form/form.component.ts
+++ src/app/component/form/form.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, OnDestroy } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Applicant } from 'app/model/applicant';
 import { ApplicantService } from 'app/service/applicant.service';
+import { stringify } from 'querystring';
+import { map, pipe, Subscription } from 'rxjs';
+import { SettingService } from '../../service/setting.service';
+import { OriginalForm } from '../../model/form';
 
 @Component({
   selector: 'ia-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss'],
 })
-export class FormComponent implements OnInit {
+export class FormComponent implements OnInit, OnDestroy, AfterViewInit {
   applicant: Applicant = {
     media: '',
     name: '',
@@ -16,13 +21,46 @@ export class FormComponent implements OnInit {
   reader = new FileReader();
   fileBlog: string | ArrayBuffer = '';
   fileName: string;
+  formGroup: FormGroup;
+  formsSubscription: Subscription;
 
   constructor(
     private applicantService: ApplicantService,
-    private router: Router
-  ) {}
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private settingService: SettingService
+  ) {
+    this.formsSubscription = this.settingService.forms$
+      .pipe(
+        map((forms) => {
+          return forms.map((form) => form.name);
+        })
+      )
+      .subscribe((formNames) => {
+        // const group = {};
+        // formNames.forEach((name) => {
+        //   group[name] = new FormControl('');
+        // });
+        // console.log(group);
+        // this.formGroup = new FormGroup(group);
+      });
+
+    this.formGroup = this.formBuilder.group({
+      name: ['', []],
+      tel: ['', []],
+      email: ['', []],
+    });
+  }
 
   ngOnInit(): void {}
+
+  ngAfterViewInit(): void {
+    const c = this.formGroup.valueChanges.subscribe((v) => console.log(v));
+  }
+
+  ngOnDestroy(): void {
+    this.formsSubscription.unsubscribe();
+  }
 
   addApplicant(): void {
     this.applicantService
