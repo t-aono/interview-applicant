c6c620f8b23bdfbd8e550885b0a336e26cb34575
t-aono <46856574+t-aono@users.noreply.github.com>
2024-05-13 12:26:09 +0900

    Add emulator setting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,23 @@ import { DetailComponent } from './component/detail/detail.component';
 import { environment } from '../environments/environment';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideAuth, getAuth } from '@angular/fire/auth';
+import {
+  connectFirestoreEmulator,
+  getFirestore,
+  provideFirestore,
+} from '@angular/fire/firestore';
+import {
+  connectStorageEmulator,
+  getStorage,
+  provideStorage,
+} from '@angular/fire/storage';
 import { FormComponent } from './component/form/form.component';
 import { DoneComponent } from './component/done/done.component';
 import { LoginComponent } from './component/login/login.component';
 import { ListComponent } from './component/list/list.component';
 import { SettingComponent } from './component/setting/setting.component';
 import { DateStringPipe } from './pipe/date-string.pipe';
+import { connectAuthEmulator } from 'firebase/auth';
 
 @NgModule({
   declarations: [
@@ -35,7 +46,29 @@ import { DateStringPipe } from './pipe/date-string.pipe';
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth()),
+    provideAuth(() => {
+      const auth = getAuth();
+      if (environment.id === 'local') {
+        connectAuthEmulator(auth, 'http://localhost:9099', {
+          disableWarnings: true,
+        });
+      }
+      return auth;
+    }),
+    provideFirestore(() => {
+      const firestore = getFirestore();
+      if (environment.id === 'local') {
+        connectFirestoreEmulator(firestore, 'localhost', 8080);
+      }
+      return firestore;
+    }),
+    provideStorage(() => {
+      const storage = getStorage();
+      if (environment.id === 'local') {
+        connectStorageEmulator(storage, 'localhost', 9199);
+      }
+      return storage;
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
