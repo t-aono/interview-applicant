d454f2bf83fa54c1a102a966e440bc44ea3100be
t-aono <46856574+t-aono@users.noreply.github.com>
2024-05-13 18:37:31 +0900

    Fix setting service

--- src/app/service/setting.service.ts
+++ src/app/service/setting.service.ts
@@ -1,33 +1,38 @@
 import { Injectable, inject } from '@angular/core';
 import { Observable, map, take } from 'rxjs';
 import { OriginalForm } from 'app/model/form';
+import { Firestore } from '@angular/fire/firestore';
 import {
-  DocumentReference,
-  Firestore,
-  addDoc,
-  collection,
-  collectionData,
-  doc,
-  updateDoc,
-} from '@angular/fire/firestore';
+  AngularFirestore,
+  AngularFirestoreCollection,
+} from '@angular/fire/compat/firestore';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SettingService {
   firestore: Firestore = inject(Firestore);
-  formsCollection = collection(this.firestore, 'forms');
+  private formsCollection: AngularFirestoreCollection<OriginalForm>;
   forms$: Observable<OriginalForm[]>;
   formsCount = 0;
 
-  constructor() {
-    this.forms$ = (
-      collectionData(this.formsCollection) as Observable<OriginalForm[]>
-    ).pipe(map((data) => data.sort((a, b) => a.key - b.key)));
+  constructor(private afs: AngularFirestore) {
+    this.formsCollection = afs.collection<OriginalForm>('forms');
+    this.forms$ = this.formsCollection.snapshotChanges().pipe(
+      map((actions) =>
+        actions
+          .map((a) => {
+            const data = a.payload.doc.data() as OriginalForm;
+            const id = a.payload.doc.id;
+            return { id, ...data };
+          })
+          .sort((a, b) => a.key - b.key)
+      )
+    );
   }
 
   addForm(form: OriginalForm) {
-    addDoc(this.formsCollection, form);
+    this.formsCollection.add(form);
   }
 
   setFormsCount(count: number) {
@@ -38,8 +43,7 @@ export class SettingService {
     this.forms$.pipe(take(1)).subscribe((forms) =>
       forms.forEach((form) => {
         if (form.key === newForm.key) {
-          const document = doc(this.formsCollection, form.id);
-          updateDoc(document as DocumentReference<OriginalForm>, newForm);
+          this.formsCollection.doc(form.id).update(newForm);
         }
       })
     );
